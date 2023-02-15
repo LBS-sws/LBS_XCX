c0b77a0888793183f007abfdeef4660440e617bd
xiangsong <wait973@qq.com>
2023-02-15 15:35:16 +0800

    调整 风险跟进数据数据查询缓慢的问题。

--- app/technician/controller/Getlastrisks.php
+++ app/technician/controller/Getlastrisks.php
@@ -1,7 +1,8 @@
 <?php
-declare (strict_types = 1);
+declare (strict_types=1);
 
 namespace app\technician\controller;
+
 use app\BaseController;
 use think\facade\Db;
 use think\facade\Request;
@@ -16,92 +17,79 @@ class Getlastrisks
         $result['data'] = null;
 
         $token = request()->header('token');
-        if(!isset($_POST['staffid']) || !isset($token) || !isset($_POST['job_id']) || !isset($_POST['job_type'])){
-            return json($result); 
+        if (!isset($_POST['staffid']) || !isset($token) || !isset($_POST['job_id']) || !isset($_POST['job_type'])) {
+            return json($result);
         }
-        if(empty($_POST['staffid']) || empty($token) || empty($_POST['job_id']) || empty($_POST['job_type'])){
-            return json($result); 
+        if (empty($_POST['staffid']) || empty($token) || empty($_POST['job_id']) || empty($_POST['job_type'])) {
+            return json($result);
         }
         //获取信息
         $staffid = $_POST['staffid'];
         $job_id = $_POST['job_id'];
         $job_type = $_POST['job_type'];
         //获取用户登录信息
-        $user_token = Db::name('token')->where('StaffID',$staffid)->find();
+        $user_token = Db::name('token')->where('StaffID', $staffid)->find();
         $login_time = strtotime($user_token['stamp']);
         $now_time = strtotime('now');
-        $c_time = ($now_time - $login_time)/60/60;
+        $c_time = ($now_time - $login_time) / 60 / 60;
         //验证登录状态
-        if ($token==$user_token['token'] &&  ($c_time <= 24)) {
+        if ($token == $user_token['token'] && ($c_time <= 24)) {
             $wheres['job_id'] = $job_id;
             $wheres['job_type'] = $job_type;
             $last_risk_datas = array();
             //查询当前服务
             if ($job_type == 1) {
-                $job = Db::table('joborder')->where('JobID',$job_id)->field('ContractID,ServiceType')->find();
+                $job = Db::table('joborder')->where('JobID', $job_id)->field('ContractID,ServiceType')->find();
                 $last_w['ContractID'] = $job['ContractID'];
                 $last_w['ServiceType'] = $job['ServiceType'];
-                $last_w['Status'] = 3 ;
-                $last_job =  Db::table('joborder')->where($last_w)->order('JobDate', 'desc')->field('JobID as id')->select();
-            }else if ($job_type == 2) {
-                $job = Db::table('followuporder')->where('FollowUpID',$job_id)->field('ContractID,SType')->find();
-                $last_w['ContractID'] = $job['ContractID'];
-                $last_w['SType'] = $job['SType'];
-                $last_w['Status'] = 3 ;
-                $last_job =  Db::table('followuporder')->where($last_w)->order('JobDate', 'desc')->field('FollowUpID as id')->select();
+                $last_w['Status'] = 3;
+                $last_e['job_type'] = 1;
+
+                // 获取所有存在的id
+                $last_job = Db::table('joborder')->where($last_w)->order('JobDate', 'desc')->field('GROUP_CONCAT(JobID) as id')->find();
+            } else if ($job_type == 2) {
+                // 第二种情况应该是不存在的   暂时不管
+                // $job = Db::table('followuporder')->where('FollowUpID',$job_id)->field('ContractID,SType')->find();
+                // $last_w['ContractID'] = $job['ContractID'];
+                // $last_w['SType'] = $job['SType'];
+                // $last_w['Status'] = 3 ;
+                // $last_e['job_type'] = 2;
+
+                // $last_job =  Db::table('followuporder')->where($last_w)->order('JobDate', 'desc')->field('FollowUpID as id')->find();
+                $result['code'] = 1;
+                $result['msg'] = '成功';
+                $result['data'] = [];
+                return json($result);
             }
-            if (!empty($last_job)) {  
-                $y_array = [] ;
-                $n_array = [] ;
-                $f_array = [] ;
-                for($i=0;$i<count($last_job);$i++){
-                    $last_e['job_id'] = $last_job[$i]['id'];
-                    $last_e['job_type'] = $job_type;
-                    $y = Db::table('lbs_service_risks')->where($last_e)->where('status',1)->where('follow_id',0)->order('id', 'asc')->select()->toArray();
-                    $n = Db::table('lbs_service_risks')->where($last_e)->where('status',0)->where('follow_id',0)->order('id', 'asc')->select()->toArray();
-                    $risk_id_datas = Db::table('lbs_service_risks')->where('job_id',$job_id)->where('job_type',$job_type)->order('id', 'asc')->field('id')->select()->toArray();
-                    $risk_ids = [] ;
-                    if (count($risk_id_datas)>0) {
-                        for($r=0; $r < count($risk_id_datas); $r++){
-                            array_push($risk_ids,$risk_id_datas[$r]['id']);
-                        }
-                    }
-                    $f = Db::table('lbs_service_risks')->where($last_e)->where('status',2)->whereIn('follow_id',$risk_ids)->order('id', 'asc')->select()->toArray();
-                    if (count($y)>0) {
-                        for ($m=0; $m < count($y); $m++) { 
-                            array_push($y_array,$y[$m]);
-                        }
-                    }
-                    if (count($n)>0) {
-                        for ($j=0; $j < count($n); $j++) { 
-                            array_push($n_array,$n[$j]);
-                        }
-                    }
-                    if (count($f)>0) {
-                        for ($k=0; $k < count($f); $k++) { 
-                            array_push($f_array,$f[$k]);
-                        }
-                    }
-                }    
+            $y = [];
+            $n = [];
+            $f = [];
+            if (isset($last_job) && $last_job['id'] != null) {
+                $y = Db::table('lbs_service_risks')->where($last_e)->where('status', 1)->where('follow_id', 0)->whereIn('job_id', $last_job['id'])->order('id', 'asc')->select();
+                $n = Db::table('lbs_service_risks')->where($last_e)->where('status', 0)->where('follow_id', 0)->whereIn('job_id', $last_job['id'])->order('id', 'asc')->select();
+                $f = Db::table('lbs_service_risks')->where($last_e)->where('status', 2)->whereIn('job_id', $last_job['id'])->where('follow_id', '>', 0)->order('id', 'asc')->select();
             }
-            if (count($n_array)>0) {
-                $last_risk_datas['n'] = $n_array;
+            $last_risk_datas['y'] = [];
+            $last_risk_datas['n'] = [];
+            $last_risk_datas['f'] = [];
+            if (count($y) > 0) {
+                $last_risk_datas['y'] = $y ?? 0;
             }
-            if (count($y_array)>0) {
-                $last_risk_datas['y'] = $y_array;
+            if (count($n) > 0) {
+                $last_risk_datas['n'] = $n ?? 0;
             }
-            if (count($f_array)>0) {
-                $last_risk_datas['f'] = $f_array;
+            if (count($f) > 0) {
+                $last_risk_datas['f'] = $f ?? 0;
             }
-             //返回数据
+            //返回数据
             $result['code'] = 1;
             $result['msg'] = '成功';
             $result['data'] = $last_risk_datas;
-           
-        }else{
-             $result['code'] = 0;
-             $result['msg'] = '登录失效，请重新登陆';
-             $result['data'] = null;
+
+        } else {
+            $result['code'] = 0;
+            $result['msg'] = '登录失效，请重新登陆';
+            $result['data'] = null;
         }
         return json($result);
     }
