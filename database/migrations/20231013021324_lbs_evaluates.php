3c815d3e4328cbf05835602fedadd0de3a3928dc
anlondon@foxmail.com <gitlabpassword>
2023-10-23 13:07:41 +0800

    fix: 为点评表添加触发器，并修改字段

--- database/migrations/20231013021324_lbs_evaluates.php
+++ database/migrations/20231013021324_lbs_evaluates.php
@@ -25,10 +25,44 @@ final class LbsEvaluates extends AbstractMigration
               KEY `idx` (`order_id`,`order_type`) USING BTREE
             ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COMMENT='客户签名评价表';
         ");
+
+        //修改字段
+        Db::execute("
+            ALTER TABLE `lbs_xcx`.`lbs_evaluates` MODIFY COLUMN `question` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '问题内容' AFTER `id`;
+        ");
+
+        //添加触发器
+        Db::execute("
+            CREATE TRIGGER `trg_ins_lbs_evaluates` AFTER INSERT ON `lbs_evaluates` FOR EACH ROW BEGIN
+              INSERT INTO datasync.sync_data_log(db_id, tab_id, action_id, json_data)
+              VALUES ('lbs_xcx', 'lbs_evaluates', 'insert', JSON_OBJECT(
+                'id',NEW.id,'question',NEW.question,'score',NEW.score,'total_score',NEW.total_score,'staff_id',NEW.staff_id,'order_id',NEW.order_id,'order_type',NEW.order_type,'customer_id',NEW.customer_id,'create_time',NEW.create_time,'update_time',NEW.update_time
+              ));
+            END;
+            
+            CREATE TRIGGER `trg_upd_lbs_evaluates` AFTER UPDATE ON `lbs_evaluates` FOR EACH ROW BEGIN
+              INSERT INTO datasync.sync_data_log(db_id, tab_id, action_id, json_data)
+              VALUES ('lbs_xcx', 'lbs_evaluates', 'update', JSON_OBJECT(
+                'id',NEW.id,'question',NEW.question,'score',NEW.score,'total_score',NEW.total_score,'staff_id',NEW.staff_id,'order_id',NEW.order_id,'order_type',NEW.order_type,'customer_id',NEW.customer_id,'create_time',NEW.create_time,'update_time',NEW.update_time
+              ));
+            END;
+            
+            CREATE TRIGGER `trg_del_lbs_evaluates` BEFORE DELETE ON `lbs_evaluates` FOR EACH ROW BEGIN
+              INSERT INTO datasync.sync_data_log(db_id, tab_id, action_id, json_data)
+              VALUES ('lbs_xcx', 'lbs_evaluates', 'delete', JSON_OBJECT(
+                'id',NEW.id,'question',NEW.question,'score',NEW.score,'total_score',NEW.total_score,'staff_id',NEW.staff_id,'order_id',NEW.order_id,'order_type',NEW.order_type,'customer_id',NEW.customer_id,'create_time',NEW.create_time,'update_time',NEW.update_time
+              ));
+            END;
+        ");
     }
 
     public function down(): void
     {
         Db::execute('DROP TABLE lbs_evaluates');
+
+        //修改字段
+        Db::execute("
+            ALTER TABLE `lbs_xcx`.`lbs_evaluates` MODIFY COLUMN `question` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '问题内容' AFTER `id`;
+        ");
     }
 }
