77812133b9ced32a57441d5cf2a94e8ced2e693b
xiangsong <xiangsong.cd@lbsgroup.com.cn>
2023-11-02 20:45:11 +0800

    批量保存点评。

--- app/common/model/JobOrder.php
+++ app/common/model/JobOrder.php
@@ -15,4 +15,24 @@ class JobOrder extends Model
     public function ReportAutographV2(){
         return $this->hasOne(AutographV2::class,'job_id', 'JobID')->bind(['customer_grade']);
     }
+
+    public function checkOrders($condition): array
+    {
+        //根据工作id查询出客户编号是多少
+        $where = [
+            'JobDate' => $condition['job_date'],
+            'CustomerID' => $condition['customer_id'],
+            'Staff01' => $condition['staff_id'],
+            ['JobID', '<>', $condition['order_id']],
+            ['StartTime', '<>', '00:00:00']
+        ];
+        $orders = JobOrder::field('JobID')
+            ->where($where)
+            ->select();
+
+        if ($orders->isEmpty()) {
+            return [];
+        }
+        return $orders->toArray();
+    }
 }
--- app/technician/controller/Evaluates.php
+++ app/technician/controller/Evaluates.php
@@ -6,6 +6,8 @@ use app\Request;
 use app\common\model\AutographV2;
 use app\common\model\FollowupOrder;
 use app\common\model\JobOrder;
+use Exception;
+use think\facade\Db;
 use think\response\Json;
 
 /**
@@ -36,6 +38,8 @@ class Evaluates
         return success(1,'成功',$data);
     }
 
+
+
     /**
      * 新增评价记录
      * @param Request $request
@@ -43,69 +47,126 @@ class Evaluates
      */
     public function add(Request $request): Json
     {
-        $questions_str =  $request->post('questions','');//问题
-        $questionType =  $request->post('question_type','questions');//问题类型
-        $staffId =  $request->post('staffid','');//职员id
-        $jobId =  $request->post('job_id',0);//订单id
-        $jobType =  $request->post('job_type',0);//订单id
+        $questions_str = $request->post('questions', ''); //问题
+        $questionType = $request->post('question_type', 'questions'); //问题类型
+        $staffId = $request->post('staffid', ''); //职员id
+        $jobId = $request->post('job_id', 0); //订单id
+        $jobType = $request->post('job_type', 0); //订单id
 
-        if(empty($staffId) || empty($jobId) || empty($jobType)){
-            return error(0,'缺少参数');
+        if (empty($staffId) || empty($jobId) || empty($jobType)) {
+            return error(0, '缺少参数');
         }
 
         //是否已评价过
-        $evaluates = (new \app\technician\model\Evaluates())->where(['order_id'=>$jobId,'order_type'=>$jobType])->find();
-        if(empty($evaluates)){
-            $evaluates = new \app\technician\model\Evaluates();
-        }
+        $evaluates = \app\technician\model\Evaluates::where(['order_id' => $jobId, 'order_type' => $jobType])->findOrEmpty();
 
         //查询订单
-        $customer_id = '';
-        switch ($jobType){
+//        $customer_id = '';
+//        $job_date = '';
+//        $staff_id = '';
+
+        switch ($jobType) {
             case 1: //jobOrder
-                $Order =  JobOrder::field('JobID,CustomerID')->Where('JobID',$jobId)->find();
-                if(empty($Order)){
-                    return error(0,'找不到工作单');
-                }
-                $customer_id = $Order['CustomerID'];
+                $Order = JobOrder::field('JobID,CustomerID,JobDate,Staff01')->Where('JobID', $jobId)->findOrEmpty();
                 break;
             case 2: //followUpOrder
-                $Order =  FollowupOrder::field('FollowUpID,CustomerID')->Where('FollowUpID',$jobId)->find();
-                if(empty($Order)){
-                    return error(0,'找不到跟进单');
-                }
-                $customer_id = $Order['CustomerID'];
+                $Order = FollowupOrder::field('FollowUpID,CustomerID,JobDate,Staff01')->Where('FollowUpID', $jobId)->findOrEmpty();
                 break;
+            default:
+                return error(0, '无效的订单类型');
         }
 
+        if ($Order->isEmpty()) {
+            return error(0, '找不到订单');
+        }
+
+        $customer_id = $Order['CustomerID'];
+        $job_date = $Order['JobDate'];
+        $staff_id = $Order['Staff01'];
+
         //整理得分
-        $questions = config('evaluates.'.$questionType);//获取原题
-        $user_answer = json_decode($questions_str,true);
+        $questions = config('evaluates.' . $questionType); //获取原题
+        $user_answer = json_decode($questions_str, true);
         $total_score = count($questions);
         $score = 0;
-        foreach ($questions as $key=>$val){
-            if($val['type'] == 'radio' && $user_answer[$key]['answer']==1){//单选项 且
+
+        foreach ($questions as $key => $val) {
+            if ($val['type'] == 'radio' && $user_answer[$key]['answer'] == 1) { //单选项 且
                 $score++;
             }
         }
 
-        //保存
-        $evaluates->save([
-           'question' => $questions_str,
-           'score' => $score,
-           'total_score' => $total_score,
-           'customer_id' => $customer_id,
-           'staff_id' => $staffId,
-           'order_id' => $jobId,
-           'order_type' => $jobType
-        ]);
+        $condition = [
+            'job_date' => $job_date,
+            'staff_id' => $staff_id,
+            'customer_id' => $customer_id,
+            'order_id' => $jobId,
+        ];
 
+        // 在这里执行检查有哪些订单符合条件
+
+        // 原始准备保存的数据
+        $originalOrder = [
+            'question' => $questions_str,
+            'score' => $score,
+            'total_score' => $total_score,
+            'customer_id' => $customer_id,
+            'staff_id' => $staffId,
+            'order_id' => $jobId,
+            'order_type' => $jobType,
+        ];
+
+        // 在这里执行检查有哪些订单符合条件
+        $moreOrder = $this->checkOrders($condition);
+        if (count($moreOrder) > 0 && $jobType == 1) {
+            $newOrder = [];
+            foreach ($moreOrder as $k => $v) {
+                $newOrder[$k] = [
+                    'order_id' => $v['JobID'],
+                    'question' => $questions_str,
+                    'score' => $score,
+                    'total_score' => $total_score,
+                    'customer_id' => $customer_id,
+                    'staff_id' => $staffId,
+                    'order_type' => $jobType,
+                ];
+            }
+            // 把本身的追加进去
+            $newOrder[] = $originalOrder;
+            // 批量更新或新增
+            foreach ($newOrder as $order) {
+                $evaluates->where(['order_id' => $order['order_id']])->findOrEmpty()->save($order);
+            }
+            $jobId = array_column($newOrder, 'order_id');
+
+        } else {
+            // 单个保存
+            $evaluates->save($originalOrder);
+        }
         //更新 lbs_report_autograph_v2 的评分
-        (new AutographV2())->where(['job_id'=>$jobId,'job_type'=>$jobType])->save(['customer_grade'=>$score]);
-
-        return success(1, '点评成功');
+        $res = AutographV2::whereIn('job_id',$jobId)->where(['job_type' => $jobType])->update(['customer_grade' => $score]);
+        if($res) return success(1, '点评成功');
     }
 
+    public function checkOrders($condition): array
+    {
+        //根据工作id查询出客户编号是多少
+        $where = [
+            'JobDate' => $condition['job_date'],
+            'CustomerID' => $condition['customer_id'],
+            'Staff01' => $condition['staff_id'],
+            ['JobID', '<>', $condition['order_id']],
+            ['StartTime', '<>', '00:00:00']
+        ];
+        $orders = JobOrder::field('JobID')
+            ->where($where)
+            ->select();
+
+        if ($orders->isEmpty()) {
+            return [];
+        }
+        return $orders->toArray();
+    }
     /**
      * 获取问卷与填写详情
      * @return void
