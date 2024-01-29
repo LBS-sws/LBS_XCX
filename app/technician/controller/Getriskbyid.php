d629c645b5d8b0e3c3359826068649d32e1866e8
6058841891 <yiming.cd@lbsgroup.com.cn>
2024-01-29 17:58:42 +0800

    风险跟进详情页更新   查询客户类型   248KA客户

--- app/technician/controller/Getriskbyid.php
+++ app/technician/controller/Getriskbyid.php
@@ -17,10 +17,10 @@ class Getriskbyid
 
         $token = request()->header('token');
         if(!isset($_POST['staffid']) || !isset($token) || !isset($_POST['job_id']) || !isset($_POST['job_type']) || !isset($_POST['city']) || !isset($_POST['service_type'])){
-            return json($result); 
+            return json($result);
         }
         if(empty($_POST['staffid']) || empty($token) || empty($_POST['job_id']) || empty($_POST['job_type']) || empty($_POST['city']) || empty($_POST['service_type'])){
-            return json($result); 
+            return json($result);
         }
         //获取信息
         $staffid = $_POST['staffid'];
@@ -34,6 +34,8 @@ class Getriskbyid
         $login_time = strtotime($user_token['stamp']);
         $now_time = strtotime('now');
         $c_time = ($now_time - $login_time)/60/60;
+
+        $CustomerType = '';
         //验证登录状态
         if ($token==$user_token['token'] &&  ($c_time <= 24*30)) {
             $wheres['id'] = $job_id;
@@ -42,19 +44,25 @@ class Getriskbyid
             $service_data['risk'] = [];
             if($id>0){
                 $service_data['risk'] = Db::table('lbs_service_risks')->where('id',$id)->find();
+
+                $CustomerID = Db::table('joborder')->where('JobID',$service_data['risk']['job_id'])->value('CustomerID');
+
+                $CustomerType = Db::table('customercompany')->where('CustomerID',$CustomerID)->value('CustomerType');
             }
             $service_data['targets'] =  Db::table('lbs_service_risk_target_lists')->where('city',$city)->field('target as label,target as value')->select();
             $service_data['types'] =  Db::table('lbs_service_risk_type_lists')->where('city',$city)->field('type as label,type as value')->select();
             $service_data['ranks'] =  Db::table('lbs_service_risk_rank_lists')->field('rank as label,rank as value')->select();
             $service_data['labels'] =  Db::table('lbs_service_risk_label_lists')->field('label as label,label as value')->select();
+            $service_data['CustomerType'] = $CustomerType;
+
             $result['code'] = 1;
             $result['msg'] = '成功';
             $result['data'] = $service_data;
-           
+
         }else{
-             $result['code'] = 0;
-             $result['msg'] = '登录失效，请重新登陆';
-             $result['data'] = null;
+            $result['code'] = 0;
+            $result['msg'] = '登录失效，请重新登陆';
+            $result['data'] = null;
         }
         return json($result);
     }
